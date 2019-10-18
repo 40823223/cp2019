1e174c86c1007f159c48aabbc3188d5fb295dc0f
40823223 <40823223@mde.tw>
2019-10-19 00:54:39 +0800

    intital add

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '\xa0 https://dartpad.dev/embed-inline.html?id=28f4be618869b6f51fce0d446f350714 \xa0 \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '\xa0 https://dartpad.dev/embed-inline.html?id=28f4be618869b6f51fce0d446f350714 \xa0 \n \n', 'tags': '', 'url': 'About.html'}]};
